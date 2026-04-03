5e8d428249d11c0d78964e5e4d8e8a463fa479ba
xujian <xujian19930618@outlook.com>
2026-04-03 14:06:41 +0800

    更改

---
+++ lib/component/CenterDemo.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CenterDemo extends StatefulWidget {
+  const CenterDemo({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    debugPrint("创建阶段: createState");
+    return _CenterDemoState();
+  }
+}
+
+class _CenterDemoState extends State<CenterDemo> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw MaterialApp(
+      title: "Flutter",
+      home: Scaffold(
+        appBar: AppBar(title: Text("头部区域")),
+        body: Center(child: Text("中部区域")),
+        bottomNavigationBar: SizedBox(
+          height: 80,
+          child: Center(child: Text("底部区域")),
+        ),
+      ),
+    );
+  }
+}
